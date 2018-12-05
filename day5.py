a5c0acc1ee0c999ccb77ccc00598017769e881fe
emanuelfeld <emanuelfeld@gmail.com>
2018-12-05 11:35:29 -0500

    speed up 5

--- day5.py
+++ day5.py
@@ -1,26 +1,11 @@
-import re
 import string
 
-with open(f'input/day5.txt') as infile:
+with open('input/day5.txt') as infile:
     inp = infile.readline().strip()
 
-toggle_case = {}
-for lower in string.ascii_lowercase:
-    upper = lower.upper()
-    toggle_case[lower] = upper
-    toggle_case[upper] = lower
-
-
-def part1(s):
-    stack = []
-    for char in s:
-        if stack and char == toggle_case[stack[-1]]:
-            stack.pop()
-        else:
-            stack.append(char)
-    return len(stack)
-
 # SLOW part 1
+
+# import re
 
 # pairs = '|'.join([f'{c}{c.upper()}|{c.upper()}{c}' for c in string.ascii_lowercase])
 # pattern = re.compile(pairs)
@@ -35,14 +20,28 @@ def part1(s):
 #             l = len(s)
 #     return l
 
+toggle_case = {}
+for lower in string.ascii_lowercase:
+    upper = lower.upper()
+    toggle_case[lower] = upper
+    toggle_case[upper] = lower
+
+def part1(s):
+    stack = []
+    for char in s:
+        if stack and char == toggle_case[stack[-1]]:
+            stack.pop()
+        else:
+            stack.append(char)
+    return stack
 
 def part2(s):
     lengths = []
     for char in string.ascii_lowercase:
-        s_no_char = re.sub(char, '', s, flags=re.IGNORECASE)
-        lengths.append(part1(s_no_char))
+        s_no_char = [c for c in s if c.lower() != char]
+        lengths.append(len(part1(s_no_char)))
     return min(lengths)
 
-
-print(part1(inp))
-print(part2(inp))
+reduced = part1(inp)
+print(len(reduced))
+print(part2(reduced))
